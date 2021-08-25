4be86042ef84c591d5401808503078e528754808
estesca11 <cjo7897@gmail.com>
2021-08-25 16:28:17 +0900

    merged

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
+var path = requiure('path');
 var template = require('./lib/template.js');
 
 var app = http.createServer(function (request, response) {
